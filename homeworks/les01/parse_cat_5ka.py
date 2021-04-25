52e6acf5baaf7e2bee5eb84ce0bc95bf4cbcddf3
Alex Klyanchin <k142@mail.ru>
2021-04-26 00:36:26 +0300

    les01 working!

--- homeworks/les01/parse_cat_5ka.py
+++ homeworks/les01/parse_cat_5ka.py
@@ -2,80 +2,58 @@ import json
 import time
 from pathlib import Path
 import requests
+import homeworks.les01.parse_5ka as p5ka
 
 """
 https://5ka.ru/special_offers/
-- Извлеч все доступные данные о товарах
-- Каждый товар положить в отдельный файл в формате JSON
+
+Задача организовать сбор данных, 
+необходимо иметь метод сохранения данных в .json файлы
+результат: Данные скачиваются с источника, при вызове метода/функции сохранения в файл скачанные данные 
+сохраняются в Json вайлы, для каждой категории товаров должен быть создан отдельный файл и содержать товары 
+исключительно соответсвующие данной категории.
+
+пример структуры данных для файла: 
+нейминг ключей можно делать отличным от примера
+
+{
+"name": "имя категории",
+"code": "Код соответсвующий категории (используется в запросах)",
+"products": [{PRODUCT}, {PRODUCT}........] # список словарей товаров соответсвующих данной категории
+}
 """
 
 
-class Parse5ka:
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0"
-    }
-    params = {
-        # максимальное значение
-        "records_per_page": 20,
-        # выход по тайм-ауту из GET
-        # "timeout": 5,
-    }
-
-    def __init__(self, star_url: str, save_path: Path):
-        self.star_url = star_url
-        self.save_path = save_path
-
-    def _get_response2(self, url, *args, **kwargs):
-        while True:
-            # контроль по тайм-ауту
-            try:
-                response = requests.get(url, *args, **kwargs)
-                if response.status_code == 200:
-                    return response
-                time.sleep(3)
-            finally:
-                pass
-
-    def _get_response(self, url, *args, **kwargs):
-        while True:
-            response = requests.get(url, *args, **kwargs)
-            if response.status_code == 200:
-                return response
-            time.sleep(3)
-
+class ParseCat5ka(p5ka.Parse5ka):
     def run(self):
-        for product in self._parse(self.star_url):
-            file_path = self.save_path.joinpath(f"{product['id']}.json")
-            self._save(product, file_path)
+        # список каталогов
+        response = self._get_response(self.star_url, headers=self.headers)
+        parent_cats = response.json()
+        for parcat in parent_cats:
+            # категории в каталоге
+            url = self.star_url + parcat['parent_group_code']
+            response = self._get_response(url, headers=self.headers)
+            categories = response.json()
+            for cat in categories:
+                self.params['categories']=cat['group_code']
+                for group in self._parse('https://5ka.ru/api/v2/special_offers/'):
+                    print(group)
 
     def _parse(self, url: str):
-        count = 0
         while url:
+            urln = ''
+            urln = self._clear_url(url)
             time.sleep(0.1)
-            response = self._get_response(url, headers=self.headers, params=self.params)
+            response = self._get_response(urln, headers=self.headers, params=self.params)
+            print(self.params)
             data = response.json()
             url = data["next"]
             for product in data["results"]:
                 yield product
-            count += 1
-            print(count,"->")
-            if count > 5:
-                break
-                pass
-
-    def _save(self, data: dict, file_path):
-        file_path.write_text(json.dumps(data, ensure_ascii=False))
-
-
-def get_save_path(dir_name):
-    save_path = Path(__file__).parent.joinpath(dir_name)
-    if not save_path.exists():
-        save_path.mkdir()
-    return save_path
 
 
 if __name__ == "__main__":
-    save_path = get_save_path("products")
-    url = "https://5ka.ru/api/v2/special_offers/"
-    parser = Parse5ka(url, save_path)
+    save_path = p5ka.get_save_path("products")
+    url = "https://5ka.ru/api/v2/categories/"
+    parser = ParseCat5ka(url, save_path)
     parser.run()
